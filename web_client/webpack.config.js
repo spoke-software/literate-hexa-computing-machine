0c70e759d4eb76e6c0f7530f6ce1d6135ce585a5
Sean M Hill <smhill818@gmail.com>
2018-01-20 21:45:43 -0500

    Minor corrections to recent commits

--- web_client/webpack.config.js
+++ web_client/webpack.config.js
@@ -54,7 +54,7 @@ module.exports = {
                 ]
             },
 
-            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+            // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
             {
                 test: /\.tsx?$/,
                 loader: 'ts-loader',
