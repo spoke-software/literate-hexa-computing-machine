7eaea0e61e67c14fb07843ac197d615b974e5714
Sean M Hill <smhill818@gmail.com>
2018-02-10 23:45:08 -0500

    Web client: add ESLint and TSLint config files.

---
+++ web_client/.eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/essential"
+    ],
+    "parser": "typescript-eslint-parser",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "plugins": [
+      "typescript"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
